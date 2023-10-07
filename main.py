73679efa042b9096d433a8f126902298b80da2c0
ricardosamm <145858657+ricardosamm@users.noreply.github.com>
2023-10-07 15:30:17 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+# matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# # print(matriz[1][2])
+# for i in matriz:
+#    for n  in i:
+#        print(i)
+
+# frutas = ['maçã', 'banana', 'uva', 'cereja', True]
+# frutas.append('melao')
+# print(frutas)
+# frutas.append(12)
+# print(frutas)
+# x = len(frutas)
+# print(x)
+
+# Exercício 1: Crie uma lista chamada numeros que contenha os números inteiros de 1 a 5 e imprima-a na tela.
+
+# numeros = [1,2,3,4,5,6]
+# print(numeros)
+  
+# for i in range(1,6):
+#   print(i)
+
+# Exercício 2: Acesse e imprima o terceiro elemento da lista numeros.
+
+# primeiro_elemento = numeros[2]
+# print(primeiro_elemento)
+
+# Exercício 3: Adicione o número 6 à lista numeros e imprima a lista atualizada.
+
+# numeros.append(6)
+# print(numeros)
+
+# Exercício 4: Remova o número 3 da lista numeros e imprima a lista resultante.
+
+# numeros.remove(3)
+# print(numeros)
+
+# Exercício 5: Crie uma lista chamada frutas contendo três nomes de frutas diferentes. Em seguida, concatene essa lista com a lista numeros e imprima o resultado.
+
+# frutas = 'cereja', 'uva', 'abacaxi'
+# print('Os numeros são', numeros , 'As frutas são ', frutas)
+
+# Exercício 6: Use um loop for para percorrer e imprimir todos os elementos da lista frutas.
+
+# for elemento in frutas:
+#     print(elemento)
+
+# Exercício 7: Verifique se o número 5 está presente na lista numeros e imprima uma mensagem informando se ele está na lista ou não.
+
+# if 5 in numeros:
+#   print('Existe existe o numero na lista')
+# else:
+#   print('Não existe o numero na lista')
